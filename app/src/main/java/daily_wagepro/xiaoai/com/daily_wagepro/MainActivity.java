987b13ab4a1f4c99e3e5c374ad80d8c31aabff36
hxh <13241292391@163.com>
2018-01-05 18:03:48 +0800

    1. 主要是更新gradle配置

--- app/src/main/java/daily_wagepro/xiaoai/com/daily_wagepro/MainActivity.java
+++ app/src/main/java/daily_wagepro/xiaoai/com/daily_wagepro/MainActivity.java
@@ -1,16 +1,7 @@
 package daily_wagepro.xiaoai.com.daily_wagepro;
 
-import android.Manifest;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.hxh.component.basicore.ui.mrecycleview.EmptyViewConfig;
-import com.hxh.component.basicore.ui.mrecycleview.MDataSource;
-import com.hxh.component.basicore.ui.mrecycleview.MRecycleView;
-import com.hxh.component.basicore.util.aspj.annotation.PermissionCheck;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,35 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initdata();
+
     }
 
-    private void initdata() {
-        MRecycleView recycleview = (MRecycleView) findViewById(R.id.recycleview);
 
-        final List<String> str  = new ArrayList<>();
-        recycleview.setAdapter(new Adap1(this))
-                .getDataRepositoryBuilder()
-                .setNoDataStateWhenRequest(new EmptyViewConfig.Build()
-                        .enableRefreshWhenClickOtherPlace()
-                        .build(), new MDataSource.NoDataCallback() {
-                    @Override
-                    public void onNoData() {
-                        str.add("12312");
-                    }
-                })
-                .fetch(str);
-    }
-
-    @PermissionCheck(permissions = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.INTERNET,
-            Manifest.permission.CAMERA,
-            Manifest.permission.READ_CONTACTS,
-            Manifest.permission.WRITE_CONTACTS,
-            Manifest.permission.ACCESS_FINE_LOCATION
-    })
     @Override
     protected void onResume() {
         super.onResume();
