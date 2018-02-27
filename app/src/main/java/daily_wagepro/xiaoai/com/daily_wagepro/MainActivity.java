b0822b1bd0758d9bb7ad2d3ef69fcf11e2be7d62
hxh <13241292391@163.com>
2018-02-27 16:06:42 +0800

    替换jcenter仓库为本地仓库
    Signed-off-by: houxiaohu <houxiaohu@xiaomi.com>

--- app/src/main/java/daily_wagepro/xiaoai/com/daily_wagepro/MainActivity.java
+++ app/src/main/java/daily_wagepro/xiaoai/com/daily_wagepro/MainActivity.java
@@ -1,15 +1,18 @@
 package daily_wagepro.xiaoai.com.daily_wagepro;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hxh.component.basicannotation.annotation.ApiServices;
+
+@ApiServices
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
 
